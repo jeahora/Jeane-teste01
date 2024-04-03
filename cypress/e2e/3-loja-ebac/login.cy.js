585302013822407af35a8ca2f58c4e34d8a29337
Jeane Hora <jeane.souza.hora@gmail.com>
2024-04-03 20:24:10 +0100

    Usando Fixture e false

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')  
+        cy.visit('minha-conta/')  
     });
 
     afterEach(() => {
@@ -15,7 +16,7 @@ describe('Funcionalidade: Login', () => {
         cy.get('#password').type('Juan020817')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, jeane.teste (não é jeane.teste? Sair)')  
-    })
+    });
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {      
         cy.get('#username').type('jey.teste@teste.com.br')
@@ -30,8 +31,27 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain' , 'Erro: A senha fornecida para o e-mail jeane.teste@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
 
-    })
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, jeane.teste (não é jeane.teste? Sair)') 
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario, { log: false })
+            cy.get('#password').type(dados.senha , { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, jeane.teste (não é jeane.teste? Sair)')
+            
+            
+        })            
+            
+
+    });   
 })
 
 
